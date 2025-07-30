c7588ddaf2c29dd6a71ac2f071546bbe75f85a88
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2025-07-30 15:12:40 +0700

    Initial commit: React app setup

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import { Alert } from "react-bootstrap";
+
+const Footer = () => {
+    return (
+        <Alert variant="info">SCM &copy; 2025</Alert>
+    );
+}
+
+export default Footer;
---
+++ src/configs/Apis.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:8080/SupplyChainManagement/api/";
+
+export const endpoints = {
+
+}
+
+export default axios.create({
+    baseURL: BASE_URL
+})
